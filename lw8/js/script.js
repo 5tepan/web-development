4385ab44f2ac4d4f61e92446f9d741115eb157df
5tepan <62357751+5tepan@users.noreply.github.com>
2020-05-30 13:01:17 +0300

    Add files via upload

---
+++ lw8/js/script.js
@@ -0,0 +1,22 @@
+const leftBtn = document.getElementById	("leftbtn");
+const rightBtn = document.getElementById("rightbtn");
+const leftBtnId = leftBtn.id;
+const rightBtnId = rightBtn.id;
+
+function slide(event) 
+{
+	const slider = document.getElementsByClassName("content_films_info")[0];
+	let transitionElem;
+	if(this.id === leftBtnId) 
+	{
+		transitionElem = slider.removeChild(slider.firstElementChild);
+		slider.append(transitionElem);
+	} else if (this.id === rightBtnId) 
+	{
+		transitionElem = slider.removeChild(slider.lastElementChild);
+		slider.prepend(transitionElem);
+	}
+}
+
+leftBtn.addEventListener('click', slide);
+rightBtn.addEventListener('click', slide);
---
+++ lw8/js/script1.js
@@ -0,0 +1,38 @@
+
+	/*const leftbtn = document.getElementById	("leftbtn");
+	 const rightbtn = document.getElementById('rightbtn');
+	 let cover = document.getElementsByClassName('content_fims_info');//Родитель для carusel_films
+	// cover = cover[0];
+	// const carusel_films = document.getElementsByClassName('slide_film');//Блоки фильмов
+	leftbtn.addEventListener('click', slide);
+	// //rightbtn.addEventListener('click', slideRight);
+	// leftbtn.addEventListener('click', slideLeft);
+	// cover.style.position = 'relative';
+
+	//cover.style.left = '0px';
+	// console.log(carusel_films);
+
+	// function slideLeft() {
+	// 	let left = cover.style.left;
+	// 	left = left.split('');
+	// 	let arr = [];
+	// 	for(let i = 0; i < left.length ; i++) {
+	// 		  if (left[i] == 'p') {
+	// 		 	break;
+	// 		 } else {
+	// 			arr[i] = left[i];
+	// 		}
+	// 	}
+	// 	arr = arr.join('');
+	// 	let next = arr - 300;
+	// 	cover.style.left = next + 'px';
+	// 	filmMoveRight();
+	// }
+	// carusel_films[0].position = 'relative';
+	// function filmMoveRight() {
+	// 	console.log(carusel_films[0]);
+		
+	// 		carusel_films[0].style.left = '-1500px';
+	// 		// let del_film = cover.removeChild(carusel_films[0]);
+	// 		// cover.append(del_film);
+	// }
